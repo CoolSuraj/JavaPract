4049ed0871a13d49f330bf44ce9b8fbffc191ceb
srjOfficial <suraj2office@duck.com>
2023-11-18 20:52:30 +0530

    session 6 & 7

---
+++ Java8Features/src/com/suraj/mains/durga/sir/lectures/Session7.java
@@ -0,0 +1,55 @@
+package com.suraj.mains.durga.sir.lectures;
+
+import java.util.function.Function;
+
+public class Session7 {
+	
+	//predicate is used for boolean check
+	public static void main(String[] args) {
+		/*
+		 * Function is something provided by functional interface 
+		 * Function->performs operation -> o/p
+		 * 4->Square Operation->16
+		 * 
+		 * inerface Function<T,R> //T is the param to function and R is return type
+		 * {
+		 * public R apply(T t);//this is the method of functional interface
+		 * }
+		 * */
+		//Simple Example 1.
+		Function<Integer,Integer > f =	i->i*i;
+		Integer sq = f.apply(4);
+		System.out.println(f.apply(55));
+		System.out.println(sq);
+		
+		//Example 2
+		Function<String,Integer> len = s->s.length() ;
+		System.out.println(len.apply("hgkjhsb hdsbsvniuob--"));
+		
+		//Example 3
+		Function<String , String> upper = s->s.toUpperCase();
+		System.out.println(upper.apply("nwe iozgp")); //making it capital
+		
+		Function<String , String> up = s->{
+			String j=s.concat("ppp-");
+			return j;
+		};
+		System.out.println(up.apply("ggg"));
+		
+		
+		//function chaining
+		System.out.println(upper.andThen(up).apply("fffffff"));
+		//now we can do the same by below lines
+		System.out.println(up.compose(upper).apply("fffffff"));
+		
+		//Now in this fashion you can use it any number of times
+		System.out.println(upper.andThen(up).andThen(up).andThen(up).andThen(up).apply("fffffff"));
+		
+		
+		
+		
+		
+		
+	}
+
+}
